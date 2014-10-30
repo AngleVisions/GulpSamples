2309f74426a54893471bad8ab6a74e2bf3c5745e
Florian Rappl <rappl.florian@gmail.com>
2014-10-30 13:01:03 +0100

    Added implicit / explicit

--- concat/gulpfile.js
+++ concat/gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var srcMask = 'src/*.js';
+var srcExplicit = ['src/a.js', 'src/b.js'];
+var source = srcMask; //Change to srcExplicit
 
 gulp.task('default', function() {
-	return gulp.src('src/*.js')
+	return gulp.src(source)
 	           .pipe(concat('app.js'))
 	           .pipe(gulp.dest('dst'));
 });
